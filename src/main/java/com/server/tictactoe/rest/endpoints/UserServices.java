fc510cfa996196d1ff6d40f5def5747b003a345d
Klaus Villaca <klausvillaca@gmail.com>
2016-01-16 22:45:42 -0500

    UserHelper and User endpoint completed

--- src/main/java/com/server/tictactoe/rest/endpoints/UserServices.java
+++ src/main/java/com/server/tictactoe/rest/endpoints/UserServices.java
@@ -3,23 +3,25 @@ package com.server.tictactoe.rest.endpoints;
 import com.server.tictactoe.Constants;
 import com.server.tictactoe.business.UserHelper;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 /**
  * Created by klausvillaca on 1/15/16.
  */
 
-@Path("/user/v1/")
+@Path("/user/v1")
 public class UserServices {
 
-    @GET
-    @Path("/userDetails/{name}")
+    /**
+     * Endpoint to create a user
+     * @param name
+     * @return
+     */
+    @PUT
+    @Path("/create/{name}")
     public Response userDetails(@PathParam("name") final String name) {
         Response resp = null;
-
         try {
             if (name != null && name != Constants.EMPTY) {
                 final UserHelper userHelper = new UserHelper();
@@ -30,7 +32,47 @@ public class UserServices {
         } catch (Exception e) {
             resp = Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
+        return resp;
+    }
 
+
+    /**
+     * Endpoint to retrieve the user
+     * @param name
+     * @return
+     */
+    @GET
+    @Path("/retrieve/{name}")
+    public Response retrieveUser(@PathParam("name") final String name) {
+        Response resp = null;
+        try {
+            if (name != null && name != Constants.EMPTY) {
+                final UserHelper userHelper = new UserHelper();
+                resp = userHelper.retrieveUser(name);
+            } else {
+                resp = Response.status(Response.Status.BAD_REQUEST).build();
+            }
+        } catch (Exception e) {
+            resp = Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+        return resp;
+    }
+
+
+    @DELETE
+    @Path("/delete/{name}")
+    public Response deleteUser(@PathParam("name") final String name) {
+        Response resp = null;
+        try {
+            if (name != null && name != Constants.EMPTY) {
+                final UserHelper userHelper = new UserHelper();
+                resp = userHelper.deleteUser(name);
+            } else {
+                resp = Response.status(Response.Status.BAD_REQUEST).build();
+            }
+        } catch (Exception e) {
+            resp = Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
         return resp;
     }
 }
